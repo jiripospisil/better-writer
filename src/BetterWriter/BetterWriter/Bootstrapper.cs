3e4c0581cc57557d8491d3502b63c964815e33a2
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-20 18:52:46 +0200

    Bring some more Prism libs

---
+++ src/BetterWriter/BetterWriter/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace BetterWriter {
+    public class Bootstrapper : UnityBootstrapper {
+        protected override DependencyObject CreateShell() {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell() {
+            base.InitializeShell();
+
+            App.Current.MainWindow = this.Shell as Window;
+            App.Current.MainWindow.Show();
+        }
+
+    }
+}
