15e3663f1a692fb9b9dddb575d3d4005b19cd68c
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-20 18:53:05 +0200

    Add main shell

--- src/BetterWriter/BetterWriter/App.xaml.cs
+++ src/BetterWriter/BetterWriter/App.xaml.cs
@@ -10,5 +10,11 @@ namespace BetterWriter {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
