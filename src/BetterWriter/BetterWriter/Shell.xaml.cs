d86c1e7beb4e8df233ae44286f4ff97e0235d885
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-20 18:51:13 +0200

    Fix class name

--- src/BetterWriter/BetterWriter/Shell.xaml.cs
+++ src/BetterWriter/BetterWriter/Shell.xaml.cs
@@ -16,8 +16,8 @@ namespace BetterWriter {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class Shell : Window {
+        public Shell() {
             InitializeComponent();
         }
     }
