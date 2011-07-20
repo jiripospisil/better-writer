92a396907f2fc6c73e12d3a9726e629668ce2234
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-20 19:06:05 +0200

    Add Editor module

---
+++ src/BetterWriter/BetterWriter.EditorModule/EditorModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+
+namespace BetterWriter.EditorModule {
+    public class EditorModule : IModule {
+
+        private readonly IUnityContainer container;
+
+        public EditorModule(IUnityContainer container) {
+            this.container = container;
+        }
+
+        public void Initialize() {
+            
+        }
+    }
+}
