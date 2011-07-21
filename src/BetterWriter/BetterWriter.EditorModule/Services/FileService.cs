32ae8cf5aa824adc8d297664e795ab6c95081a21
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-21 16:51:14 +0200

    Add IFileService and its default implementation

---
+++ src/BetterWriter/BetterWriter.EditorModule/Services/FileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BetterWriter.EditorModule.Services {
+    public class FileService : IFileService {
+        public string ReadAllFromFile(string filename) {
+            return File.ReadAllText(filename, Encoding.UTF8);
+        }
+    }
+}
