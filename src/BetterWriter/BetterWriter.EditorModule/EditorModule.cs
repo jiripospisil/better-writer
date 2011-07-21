b7f8be6b5fa6b218f88bd09c72468b1a0b1090cb
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-21 16:52:17 +0200

    Tell Unity about FileService

--- src/BetterWriter/BetterWriter.EditorModule/EditorModule.cs
+++ src/BetterWriter/BetterWriter.EditorModule/EditorModule.cs
@@ -2,17 +2,22 @@
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Prism.Regions;
 using BetterWriter.EditorModule.Views;
+using BetterWriter.EditorModule.Services;
 
 namespace BetterWriter.EditorModule {
     public class EditorModule : IModule {
 
         private readonly IRegionManager regionManager;
+        private readonly IUnityContainer container;
 
-        public EditorModule(IRegionManager regionManager) {
+        public EditorModule(IRegionManager regionManager, IUnityContainer container) {
             this.regionManager = regionManager;
+            this.container = container;
         }
 
         public void Initialize() {
+            container.RegisterType<IFileService, FileService>();
+
             regionManager.RegisterViewWithRegion("EditorModule", typeof(EditorView));
         }
     }
