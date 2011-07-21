f5ad22cb77431f00091d269c9ecf313dd471529d
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-21 17:07:48 +0200

    Fix output path and add ViewModelBase

---
+++ src/BetterWriter/BetterWriter.Common/BaseClasses/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using System.ComponentModel;
+using Microsoft.Practices.Prism.Commands;
+
+namespace BetterWriter.Common.BaseClasses {
+    public class ViewModelBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string property) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        protected void RaiseCommandCanChanged(ICommand command) {
+            DelegateCommand delegateCommand = command as DelegateCommand;
+            if(delegateCommand != null) {
+                delegateCommand.RaiseCanExecuteChanged();
+            }
+        }
+    }
+}
