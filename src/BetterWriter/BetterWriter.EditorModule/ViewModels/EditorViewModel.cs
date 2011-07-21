0a18806bb7158917000e98bc0c42228a84010b81
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-21 16:52:37 +0200

    Use FileService in EditorViewModel

--- src/BetterWriter/BetterWriter.EditorModule/ViewModels/EditorViewModel.cs
+++ src/BetterWriter/BetterWriter.EditorModule/ViewModels/EditorViewModel.cs
@@ -2,8 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BetterWriter.EditorModule.Services;
 
 namespace BetterWriter.EditorModule.ViewModels {
     public class EditorViewModel {
+
+        #region Fields
+
+        private readonly IFileService fileService;
+
+        #endregion
+
+        #region Properties with backing fields
+
+        private string _text;
+        public string Text {
+            get {
+                return _text;
+            }
+            set {
+                _text = value;
+            }
+        }
+
+        #endregion
+
+
+        #region Constructor
+
+        public EditorViewModel(IFileService fileService) {
+            this.fileService = fileService;
+        }
+
+        #endregion
+
+
+
+
     }
 }
