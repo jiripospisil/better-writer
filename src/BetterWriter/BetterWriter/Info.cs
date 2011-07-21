fee22ed947ef19b1e5879976524ad2aa0a8ed64b
Jiri Pospisil <mekishizufu@gmail.com>
2011-07-21 22:44:17 +0200

    Add Info

---
+++ src/BetterWriter/BetterWriter/Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BetterWriter {
+    public static class Info {
+        public static string GetVersion() {
+            return "0.1";
+        }
+        public static string GetName() {
+            return "Better Writer";
+        }
+    }
+}
